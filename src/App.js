52bc6aff935c082519f55fdd1d0eba99d455e371
controtie <dylan.ad.tran@gmail.com>
2016-07-14 15:06:34 -0700

    add basic scaffolding for components and containers

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Login } from './containers';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world....</h1>
+        <Login />
+      </div>  
+    );
+  }
+}

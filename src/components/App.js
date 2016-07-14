6a829c6b58e71cb50c587a575bd7879229d0735d
controtie <dylan.ad.tran@gmail.com>
2016-07-14 14:25:52 -0700

    change to simple react boilerplate

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <div>
+        <h1>Hello, world....</h1>
+        <h1>AYYYY MY MANE~!</h1>
+        <h1> coding </h1>
+      </div>  
     );
   }
 }

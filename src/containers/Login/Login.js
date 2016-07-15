8d3dce7e7b948895aa886f0687ec2ab058843b3c
controtie <dylan.ad.tran@gmail.com>
2016-07-15 15:16:27 -0700

    add responsive css to Login page

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import { LoginBar } from '../../components';
+import { LoginBar, HeaderBar } from '../../components';
 import styles from './Login.css';
 
 export default class Login extends Component {
   render() {
     return (
-      <div className={styles.loginRoot}>
-        <LoginBar />
-      </div>  
+      <div>
+        {/*<HeaderBar />*/}
+        <div className={styles.loginRoot}>
+          <LoginBar />
+        </div>  
+      </div>
     );
   }
 }

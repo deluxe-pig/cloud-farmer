2a42862f80b73a45024eececdd0c4ab24d8972e0
byeo630 <byeo630@gmail.com>
2016-07-14 20:37:44 -0700

    (refactor) Relocate HomeBody code to HomeSplash file

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { HeaderBar, HomeBody } from './../../components/index';
+import { HeaderBar, HomeBody, HomeSplash } from './../../components/index';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         <HeaderBar />
+        <HomeSplash />
         <HomeBody />
       </div>  
     );

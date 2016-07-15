61df4a9a0ed8b42e8e935917b933081782d510bf
byeo630 <byeo630@gmail.com>
2016-07-15 15:13:41 -0700

    (feat) Add styling to home splash component

--- src/components/Home/HomeSplash.js
+++ src/components/Home/HomeSplash.js
@@ -5,10 +5,14 @@ import styles from './Home.css';
 export default class HomeSplash extends Component {
   render() {
     return (
-      <div className={[styles.homeBody]}>
-        <h1>CloudFarmer</h1>
-        <p>Earn money by renting cloud storage space.</p>
-        <input type="button" value="Call to Action" className="btn" />
+      <div className={styles.homeBody}>
+        <div className={styles.splashContent}>
+          <div className={styles.splashHeader}>
+            <h1>cloudfarmer</h1>
+            <p>Earn money by renting cloud storage space.</p>
+          </div>
+          <input type="button" value="Call to Action" className="btn" />
+        </div>
       </div>
     );
   }

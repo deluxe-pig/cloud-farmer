6daf605a548217579da179aced8da631b932a1ee
controtie <dylan.ad.tran@gmail.com>
2016-07-15 10:02:25 -0700

    add basic format + styling for Login page

--- src/components/LoginBar/LoginBar.js
+++ src/components/LoginBar/LoginBar.js
@@ -7,12 +7,14 @@ export default class Login extends Component {
   render() {
     return (
       <div className={styles.loginBar}> 
-        <h1>Login</h1>
-        <form>
-        <input type="text" name="email"> </input>
-        <input type="text" name="password"> </input>
-        </form>
-        <button> Login </button>
+        <h1 className={styles.loginTitle}>Login</h1>
+        <div className={styles.formContainer}>
+          <form>
+            <input className={styles.formEntry} type="text" name="email"> </input>
+            <input className={styles.formEntry} type="text" name="password"> </input>
+            <button className={styles.formButton}> Login </button>
+          </form>
+        </div>
       </div>
     );
   }
--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { LoginBar } from '../../components';
+import styles from './Login.css';
 
 export default class Login extends Component {
   render() {
     return (
-      <div>
+      <div className={styles.loginRoot}>
         <LoginBar />
       </div>  
     );

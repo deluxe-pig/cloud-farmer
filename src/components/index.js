139bc9b91002283aa27d8a4cd2441189c1ef37ce
controtie <dylan.ad.tran@gmail.com>
2016-07-17 14:51:16 -0700

    add explanation of component/container separation logic

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,9 @@
 /**
  *  Point of contact for component modules
- *
  *  ie: import { CounterButton, InfoBar } from 'components';
  *
+ *  'Components' are ideally 100% not aware of state.
+ *  'Containers' handle all logic and pass down appropriately mapped data for components to render.
  */
 
 
@@ -15,3 +16,5 @@ export CloudDivider from './Home/CloudDivider'
 export SimpleHeader from './SimpleHeader/SimpleHeader'
 export LgTextBody from './LgTextBody/LgTextBody'
 export SimpleMeter from './SimpleMeter/SimpleMeter'
+export EditStorage from './EditStorage/EditStorage'
+
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,3 +1,9 @@
+/*
+ *
+ *  'Containers' are aware of state, handle all data manipulation, and hydrate the components they are composed of with data.
+ *
+ */
+
 export Login from './Login/Login'
 export Home from './Home/Home'
 export Faq from './Faq/Faq'

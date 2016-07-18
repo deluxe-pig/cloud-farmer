7607a9c6997be831940cdb27dabb0ad906c8e585
controtie <dylan.ad.tran@gmail.com>
2016-07-17 19:30:30 -0700

    replace button with EditStorageModal

--- src/components/EditStorage/EditStorage.js
+++ src/components/EditStorage/EditStorage.js
@@ -2,13 +2,14 @@
 import React, { Component } from 'react'
 import styles from './EditStorage.css'
 import { Link } from 'react-router'
+import { EditStorageModal } from '../index.js'
 
 export default class EditStorage extends Component {
   render() {
     return (
       <div className={styles.editStorageContainer}>
         <h1 className={styles.editStorageTitle}> </h1>
-        <button className={styles.addStorageButton}> Add Storage </button>
+        <EditStorageModal />
       </div>  
     );
   }

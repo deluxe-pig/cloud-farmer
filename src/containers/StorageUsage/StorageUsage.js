b662a8ec57d39ecc62511fe207aeac192032bca8
controtie <dylan.ad.tran@gmail.com>
2016-08-13 23:02:47 -0700

    replace EditStorage container with EditStorageModal directly

--- src/containers/StorageUsage/StorageUsage.js
+++ src/containers/StorageUsage/StorageUsage.js
@@ -1,16 +1,19 @@
 // StorageUsage.js
 import React, { Component } from 'react';
-import { SimpleMeter } from '../../components';
+import { EditStorageModal } from '../../components';
 import styles from './StorageUsage.css';
 
 export default class StorageUsage extends Component {
   render() {
     return (
       <div className={styles.storageContainer}>
-        <h2 className={styles.storageTitle}> Storage Rented </h2>
-        <SimpleMeter />
-        <h2 className={styles.storageTitle}> Storage Used </h2>
-        <SimpleMeter />
+        <h2 className={styles.storageTitle}> Storage Rented:</h2>
+        <h2>127 TB</h2>
+        <h2 className={styles.storageTitle}> Storage Used:</h2>
+        <h2>8.5 TB</h2>
+        <div className={styles.editStorageModal}>
+          <EditStorageModal />
+        </div>
       </div>
     );
   }
